8c09303c03a2926bb994ad5f9b99090c529a681a
tomo <thce28@gmail.com>
2023-02-10 14:30:01 +0100

    Try wo image filepaths

--- src/pages/Presentation/sections/data/pagesData.js
+++ src/pages/Presentation/sections/data/pagesData.js
@@ -1,27 +1,46 @@
 // import dash from "assets/images/ss-dash.png";
 // import messaging from "assets/images/ss-messaging.png";
 // import dayView from "assets/images/ss-dayView.png";
-import event from "../../../../assets/images/ss-event.png";
-import messaging from "../../../../assets/images/ss-messaging.png";
-import dayView from "../../../../assets/images/ss-dayView.png";
-import dash from "../../../../assets/images/ss-dash.png";
+// import event from "../../../../assets/images/ss-event.png";
+// import messaging from "../../../../assets/images/ss-messaging.png";
+// import dayView from "../../../../assets/images/ss-dayView.png";
+// import dash from "../../../../assets/images/ss-dash.png";
 
 // const imagesPrefix = "src/assets/images";
 export default [
   {
-    image: dash,
+    image: undefined,
     name: "Dashboard",
   },
   {
-    image: dayView,
+    image: undefined,
     name: "Calendar",
   },
   {
-    image: messaging,
+    image: undefined,
     name: "Messaging",
   },
   {
-    image: event,
+    image: undefined,
     name: "Event Screen",
   },
 ];
+
+// export default [
+//   {
+//     image: dash,
+//     name: "Dashboard",
+//   },
+//   {
+//     image: dayView,
+//     name: "Calendar",
+//   },
+//   {
+//     image: messaging,
+//     name: "Messaging",
+//   },
+//   {
+//     image: event,
+//     name: "Event Screen",
+//   },
+// ];
